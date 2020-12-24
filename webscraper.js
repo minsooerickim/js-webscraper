75b1cad30a4707122f8842554d2ff8a85e9759a5
Minsoo Kim <minsooerickim@gmail.com>
2020-12-24 13:03:38 -0800

    pc edits

--- webscraper.js
+++ webscraper.js
@@ -6,10 +6,20 @@ async function scrapeProduct(url) {
     await page.goto(url);
 
     const [el] = await page.$x('//*[@id="landingImage"]');
-    const src = el.getProperty('src');
-    const srcTxt = await src.jsonValue();
+    const src = await el.getProperty('src');
+    const imgURL = await src.jsonValue();
 
-    console.log({srcTxt});
+    const [el2] = await page.$x('//*[@id="productTitle"]');
+    const txt = await el2.getProperty('textContent');
+    const title= await txt.jsonValue();
+
+    const [el3] = await page.$x('//*[@id="price_inside_buybox"]');
+    const txt2 = await el3.getProperty('textContent');
+    const price= await txt2.jsonValue();
+
+    console.log({imgURL, title, price});
+
+    browser.close();
 }
 
 scrapeProduct('https://www.amazon.com/MSI-GeForce-Architecture-Overclocked-Graphics/dp/B08CLV8CKP/ref=sr_1_1?crid=2RR02ABIUXDRP&dchild=1&keywords=rtx+2080&qid=1608760817&sprefix=rtx%2Caps%2C235&sr=8-1')
